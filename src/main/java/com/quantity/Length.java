57b14a55216caf80da63db7450b9df6c6f69ec9c
NA20661046 <NA20661046@APL-PF30WPGM.wipro.com>
2026-02-06 16:26:35 +0530

    Added changes for feature/FeetInchesComparison

---
+++ src/main/java/com/quantity/Length.java
@@ -0,0 +1,81 @@
+package com.quantity;
+
+public class Length {
+
+	private double value;
+	private LengthUnit unit;
+
+	public enum LengthUnit {
+		FEET(12.0), INCH(1.0);
+
+		private final double conversionFactor;
+
+		LengthUnit(double conversionFactor) {
+			this.conversionFactor = conversionFactor;
+		}
+
+		public double getConversionFactor() {
+			return conversionFactor;
+		}
+
+	}
+
+	public Length(double value, LengthUnit unit) {
+		if (unit == null) {
+			throw new NullPointerException("Unit must not be null");
+		}
+
+		if (Double.isNaN(value) || Double.isInfinite(value)) {
+			throw new IllegalArgumentException("Value must be a finite number");
+		}
+
+		this.value = value;
+		this.unit = unit;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public LengthUnit getUnit() {
+		return unit;
+	}
+
+	// ---------- Convert Length to Base Unit (INCH) ----------
+	public double toBase() {
+		return unit.getConversionFactor() * value;
+	}
+
+	public boolean compare(Length thatLength) {
+		if (thatLength == null)
+			return false; // or throw IllegalArgumentException
+		final double EPSILON = 1e-6; // tolerance for floating point errors
+		return Math.abs(this.toBase() - thatLength.toBase()) < EPSILON;
+	}
+
+	// --- Equality in base unit (INCH) ---
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Length))
+			return false;
+		Length other = (Length) obj;
+		final double EPSILON = 1e-6;
+		return Math.abs(this.toBase() - other.toBase()) < EPSILON;
+	}
+
+	@Override
+	public int hashCode() {
+		final double EPSILON = 1e-6;
+		long bucket = Math.round(this.toBase() / EPSILON);
+		return Long.hashCode(bucket);
+	}
+
+	public static void main(String[] args) {
+		Length length1 = new Length(1.0, LengthUnit.FEET);
+		Length length2 = new Length(12.0, LengthUnit.INCH);
+		System.out.println("Are lenths equal? " + length1.equals(length2));
+	}
+
+}
